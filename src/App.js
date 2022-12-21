624d75f87d6dc0da1aa8a87d2b410ec4fb07c2c3
TheMVProgrammer <franklin0415@hotmail.com>
2022-12-21 14:42:36 -0400

    Assets and file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-function App() {
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => {
   return (
     <div>
-
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
